1e5e9adc0e062f1aa3362628e13884026cfdf924
Standin-Alone <johnedcelzenarosa@gmail.com>
2022-03-02 11:49:39 +0800

    push components

--- ipconfig.tsx
+++ ipconfig.tsx
@@ -1,19 +1,15 @@
 
 
 
-// export  const ipAddress = 'https://dts.da.gov.ph';
-
-
-//export  const ipAddress = 'http://172.17.150.56/dts-web/'; //IP SA OFFICE (VIP)
 
 // Bahay
-const ipAddress = 'http://192.168.1.11/dts-web';
-const ipAddress_socket = 'http://192.168.1.11';
+// const ipAddress = 'http://192.168.1.11/dts-web';
+// const ipAddress_socket = 'http://192.168.1.11';
 
 
 // Office
-// const ipAddress =  'http://172.17.150.58/evoucher/api';
-// const ipAddress_socket = 'http://172.17.150.58';
+const ipAddress =  'http://172.17.150.58/evoucher/api';
+const ipAddress_socket = 'http://172.17.150.58';
 
 // dev server
 // const ipAddress = 'https://devsysadd.da.gov.ph/evoucher/api';
